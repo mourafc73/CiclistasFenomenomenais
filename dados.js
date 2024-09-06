e5ffc5a35798dc067985ffd52b71ffd036a5e8b2
mourafc73 <56609117+mourafc73@users.noreply.github.com>
2024-09-06 17:37:18 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,28 +1,28 @@
 let dados = [
     {
         titulo: "Tadej Pogačar",
-        Pais: "Eslovênia",
+        pais: "Eslovênia",
         descricao: "Com apenas alguns anos de carreira profissional, Pogačar já conquistou múltiplas vitórias no Tour de France e em outras grandes voltas. Sua combinação de força, habilidade técnica e mentalidade vencedora o coloca entre os maiores ciclistas da história",
         link: "https://en.wikipedia.org/wiki/Tadej_Poga%C4%8Dar",
         tags: "Ciclismo Estrada Tour de France"
     },
     {
         titulo: "Mathieu van der Poel",
-        Pais: "Holanda",
+        pais: "Holanda",
         descricao: "Um verdadeiro multitalento, van der Poel domina diversas disciplinas do ciclismo. Seja em provas de um dia, clássicas ou até mesmo em campeonatos mundiais de ciclocross, ele sempre é um forte candidato ao pódio.",
         link: "https://en.wikipedia.org/wiki/Mathieu_van_der_Poel",
         tags: "Ciclocross MTB  estrada"
     },
     {
-        titulo: " Annemiek van Vleuten",
-        Pais: "Holanda",
+        titulo: "Annemiek van Vleuten",
+        pais: "Holanda",
         descricao: "A ciclista holandesa é considerada uma das maiores de todos os tempos no ciclismo feminino. Com uma série de vitórias em grandes voltas e clássicas, van Vleuten é um exemplo de força, determinação e técnica.",
         link: "https://en.wikipedia.org/wiki/Annemiek_van_Vleuten",
         tags: "Estrada  Ciclista tour de feme"
-    }
+    },
     {
         titulo: "Remco Evenepoell",
-        Pais: "Bélgica",
+        pais: "Bélgica",
         descricao: "Um verdadeiro multitalento, van der Poel domina diversas disciplinas do ciclismo. Seja em provas de um dia, clássicas ou até mesmo em campeonatos mundiais de ciclocross, ele sempre é um forte candidato ao pódio.",
         link: "https://en.wikipedia.org/wiki/Remco_Evenepoel",
         tags: "Estrada  Ouro Olimpico Contra-relógio  grandes voltas"
